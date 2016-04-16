a68cbc3035ac7e507862a15fa19280e43161e87c
juarezg <juarezg@colorado.edu>
2016-04-16 13:52:01 -0600

    Added files via upload
    Here is the start of the project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //menus
+    //add info
+    //Delete resturant
+    //list resturants, location(city), rating, type of food, price
+    //search by name
+    //If resturant not in city, shorest distance to resturant in other city
+    //Distance between similar resturants
+    //Time to city and closing times of resturants
+    //search price
+    //search by location
+    return 0;
+}
